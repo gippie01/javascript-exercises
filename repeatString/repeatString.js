7f2e01ed2923ee524ed3ab1dcacfdfdadb252e02
gippie01 <kim.taeha6@gmail.com>
2020-09-27 22:46:16 -0700

    repeatString 1/7 comment

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function(text, x) {
     let a = "";
     for (let i = 0; i < x; i++) {
-        a += text;
+        a += text; // repeat the text without any space
     }
     return a;
 }
