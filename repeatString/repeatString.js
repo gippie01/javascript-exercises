945aef2dc4d3e0d60b517c3e5d0b37ce6d2d0fdc
gippie01 <kim.taeha6@gmail.com>
2020-09-27 22:53:19 -0700

    Enabled all tests and passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,9 +1,14 @@
 const repeatString = function(text, x) {
-    let a = "";
-    for (let i = 0; i < x; i++) {
-        a += text; // repeat the text without any space
+    if (x < 0) {
+        return "ERROR";
     }
-    return a;
+    else {
+        let a = "";
+        for (let i = 0; i < x; i++) {
+            a += text; // repeat the text without any space
+        }
+        return a;
+    }
 }
 
 module.exports = repeatString
