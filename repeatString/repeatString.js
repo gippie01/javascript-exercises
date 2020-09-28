4f34ef1c04e3c831bdec948925136d3b367daca7
gippie01 <kim.taeha6@gmail.com>
2020-09-27 22:26:28 -0700

    completed helloWorld and 1/7 repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text, x) {
+    let a = "";
+    for (let i = 0; i < x; i++) {
+        a += text;
+    }
+    return a;
 }
 
 module.exports = repeatString
