b5469608a6bb46fb1df9b57b0bf88521f0ff9bd4
gippie01 <kim.taeha6@gmail.com>
2020-09-30 12:48:05 -0700

    part 1 solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+// Accept custom arrays of indefinite size
+const removeFromArray = function(...customArray, ...removeValues) {
+    // Step through the customArray
+    for (let i = 0; i < customArray.length; i++) {
+        if (customArray[i] === removeValues[i]) {
+            customArray -= customArray[i];
+        }
+    }
+    return customArray;
 }
 
 module.exports = removeFromArray
