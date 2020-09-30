e0572a7ad8c208c781d9d7d09475e9e572ff1561
gippie01 <kim.taeha6@gmail.com>
2020-09-29 21:37:47 -0700

    Complete reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    // Variable to store the reversed word
+    let reversedWord = "";
+    // Convert any input to a string to permit numbers/symbols
+    word = word.toString();
+    // Work down from last index of the input until the 0th index
+    for (let i = (word.length - 1); i >= 0; i--) {
+        // add the character stored in each index to the reversedWord
+        reversedWord += word[i];
+    }
+    // Return the reversed word
+    return reversedWord
 }
 
 module.exports = reverseString
