ee500e9b1828264f4108cb6b4d7b5eb4aa30337f
gippie01 <kim.taeha6@gmail.com>
2020-09-30 19:02:47 -0700

    Usage of rest parameters to accept multiple arguments as single array. Used forEach() and push() methods to solve the problem

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,12 +1,23 @@
-// Accept custom arrays of indefinite size
-const removeFromArray = function(...customArray, ...removeValues) {
-    // Step through the customArray
-    for (let i = 0; i < customArray.length; i++) {
-        if (customArray[i] === removeValues[i]) {
-            customArray -= customArray[i];
+/* 
+Accept multiple arguments as a single array
+(the array argument & the values to be removed)
+*/
+const removeFromArray = function(...arguments) {
+    // Get the inputted array to be manipulated from the 0th index of arg
+    const inputArray = arguments[0];
+    // New array to push the inputArray elements that aren't repeated in the arguments array
+    const newArray = [];
+
+    // Step through each element of inputArray
+    inputArray.forEach((element) => {
+        // Check if the current element in inputArray is repeated in the original arguments array
+        if (!arguments.includes(element)) {
+            // If it's not part of the arguments array, push to newArray
+            newArray.push(element);
         }
-    }
-    return customArray;
+    })
+    // Return the newArray
+    return newArray;
 }
 
 module.exports = removeFromArray
