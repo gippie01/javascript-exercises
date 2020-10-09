b6590ea38ec4307d3155c65876220611491c09cd
gippie01 <kim.taeha6@gmail.com>
2020-10-08 22:11:50 -0700

    solved

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,18 @@
-const ftoc = function() {
+// Fahrenheit to Celsius
+// Accept fahrenheit temperature
+const ftoc = function (f) {
+  // Round and return after converting with formula
+  return Math.round((f - 32) * (5 / 9) * 10) / 10;
+};
 
-}
-
-const ctof = function() {
-
-}
+// Celsius to Fahrenheit
+// Accept celsius temperature
+const ctof = function () {
+  // Round and return after converting with formula
+  return Math.round(((c * 9) / 5 + 32) * 10) / 10;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
